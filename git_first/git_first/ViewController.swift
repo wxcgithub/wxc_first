3beec54da3c0fffe51e5c4f62da672741dd7e656
lanou3g <lanou3g@lanou3gdeMac-mini-391.local>
2015-07-20 14:56:47 +0800

    hehe

---
+++ git_first/git_first/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_first
+//
+//  Created by lanou3g on 15/7/20.
+//  Copyright (c) 2015年 wangxuechao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
